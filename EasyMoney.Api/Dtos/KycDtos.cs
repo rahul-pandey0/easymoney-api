749e34d72ea1ec5ae514a83e21d978e8c3fbdd62
chayadevig72 <srinidhi.bharadwaj@techverves.in>
2026-07-31 10:28:07 +0530

    #kyc

--- EasyMoney.Api/Dtos/KycDtos.cs
+++ EasyMoney.Api/Dtos/KycDtos.cs
@@ -288,13 +288,13 @@ public record UpsertCorporateKycRequest(
     List<CorporateContactDetailRequest> ContactDetails,
 
     // Directors
-    List<CorporateDirectorRequest> Directors,
+    List<CorporateDirectorRequest> Directors
 
     // Beneficial Owners
-    List<CorporateBeneficialOwnerRequest> BeneficialOwners,
+    //List<CorporateBeneficialOwnerRequest> BeneficialOwners,
 
-    // Authorized Signatories
-    List<CorporateAuthorizedSignatoryRequest> AuthorizedSignatories
+    //// Authorized Signatories
+    //List<CorporateAuthorizedSignatoryRequest> AuthorizedSignatories
 );
 public record RegisteredOfficeRequest(
     string? AddressLine1,
